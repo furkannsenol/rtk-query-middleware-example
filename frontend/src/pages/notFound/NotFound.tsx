456b1dd294f50365e860c2e19d1cbc7050d80cde
Furkan Şenol <frkn.snl@hotmail.com>
2025-07-11 20:58:15 +0300

    Refined 404 page layout and added redirect to home

--- frontend/src/pages/notFound/NotFound.tsx
+++ frontend/src/pages/notFound/NotFound.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Button } from "reactstrap";
+import { IWithRouterProps } from "../../types/routerTypes";
+import WithRouter from "../../wrapper/WithRouter";
 
-const NotFound = () => {
+const NotFound: React.FC<IWithRouterProps> = ({ router }) => {
+  const handleBackToHome = () => {
+    router.navigate("/");
+  };
+
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="d-flex flex-column justify-content-center align-items-center text-center vh-100 gap-3">
+      <h1>Oops! Page Not Found</h1>
+      <Button color="dark" onClick={handleBackToHome}>
+        Back to Home
+      </Button>
+    </div>
+  );
+};
 
-export default NotFound
+export default WithRouter(NotFound);
